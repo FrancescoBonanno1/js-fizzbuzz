67f5d990807d9d03d11ba10993c9d7e1d1616cab
Francesco Bonanno <francescosoapbonanno@gmail.com>
2023-10-30 15:43:32 +0100

    Successione da 1 a 100 impostata con tentativo di sostituzione cifre con caratteri

---
+++ main.js
@@ -0,0 +1,9 @@
+for ( let i = 1 ; i <= 100 ; i=i++) {
+    if ( i==i*2 || i==i*5){
+        for (let i = 1 ; i<=100 ; i=i*3)
+        console.log("fizz");
+    }
+    else {
+        console.log(i);
+    }
+}
