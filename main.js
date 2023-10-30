06f780016f4eab121df8673288182ed4108db12a
Francesco Bonanno <francescosoapbonanno@gmail.com>
2023-10-30 16:35:17 +0100

    fizz e buzz implementati correttamente, manca fizzbuzz

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
-for ( let i = 1 ; i <= 100 ; i=i++) {
-    if ( i==i*2 || i==i*5){
-        for (let i = 1 ; i<=100 ; i=i*3)
+for ( let i = 1 ; i <= 100 ; i++)
+    if ( i % 3 === 0){
         console.log("fizz");
     }
+    else if (i % 5 === 0){
+        console.log("buzz");
+
+    }
+    else if (i % 15 === 3 && i % 15 === 5){
+        console.log("fizzbuzz");    
+    }
+
     else {
         console.log(i);
     }
-}
